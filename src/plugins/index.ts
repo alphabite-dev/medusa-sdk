bf8693e025c7d67a64eb98f3153ba0ce5a29415d
Ivan <ivan@alphabite.io>
2025-07-08 14:59:34 +0300

    update plugins/index exports

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,8 @@
 export * from './wishlist'
 export * from './paypal'
 export * from './reviews'
+export {
+  ProductCategoryImage,
+  ProductCollectionImage,
+  ProductVariantImage,
+} from './types'
