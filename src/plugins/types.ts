e85340e6d2589c5a864e0cfa2c54255c55669666
Ivan <ivan@alphabite.io>
2025-07-08 14:52:39 +0300

    update types

--- src/plugins/types.ts
+++ src/plugins/types.ts
@@ -16,3 +16,25 @@ export interface PaginatedOutputMeta
 }
 
 export interface PaginatedInput extends FindParams {}
+
+export interface MedusaImage {
+  id: string
+  url: string
+  rank: number
+  metadata: Record<string, unknown> | null
+  created_at: string
+  updated_at: string
+  deleted_at: string | null
+}
+
+export interface ProductCategoryImage extends MedusaImage {
+  product_category_id: string
+}
+
+export interface ProductCollectionImage extends MedusaImage {
+  product_collection_id: string
+}
+
+export interface ProductVariantImage extends MedusaImage {
+  product_variant_id: string
+}
